638f366fe41f6c5733f3f3dab5346b1633a0072b
Null Dev <NL-Dev@yandex.com>
2017-01-13 15:27:30 +0100

    Fixed col bug
    Fixed possible term color bug on other terminals/shells

--- fail2ban.sh
+++ fail2ban.sh
@@ -21,23 +21,23 @@ pkg=fail2ban
 frc=0
 printf "\n------------------------------------------------------------------\n"
 if [ "${*}" = "--help" ] || [ "${*}" = "-h" ] || [ "${*}" = "-?" ]; then
-   printf "\n\n${COL_YLW}########\n# ${COL_GRN}HELP ${COL_YLW}#\n########\n\n${COL_RST}--path  | -p PATH     :"
+   printf "\n\n${COL_YLW}########\n${COL_YLW}# ${COL_GRN}HELP ${COL_YLW}#\n${COL_YLW}########\n\n${COL_RST}--path  | -p PATH     :"
    printf "     Sets the path for the Log\n--help  | -h | -?     :     Displays this help menu"
    printf "\n--force | -f          :     Forces the start and doesn't ask for user input\n"
    printf "\n[Here will be more soon]\n\n\n"
    exit 0
 fi
 if [ "${*}" = "--force" ] || [ "${*}" = "-f" ]; then
-   printf "\n${COL_YLW}########\n# ${COL_BLE}INFO ${COL_YLW}#\n########\n\n${COL_GRN}"
-   printf "Forcing start!${COL_RST}\n"
+   printf "\n${COL_YLW}########\n${COL_YLW}# ${COL_BLE}INFO ${COL_YLW}#\n${COL_YLW}########\n\n"
+   printf "${COL_GRN}Forcing start!${COL_RST}\n"
    frc=1
    shift
 fi
 if [[ $frc != 1 ]]; then
    f2b_ok=$(dpkg-query -W --showformat='${Status}\n' $pkg|grep "install ok installed")
    if [ "" == "$f2b_ok" ]; then
-      printf "\n${COL_YLW}###########\n# ${COL_RED}WARNING ${COL_YLW}#\n###########\n\n${COL_BLE}"
-      printf "Fail2Ban is not installed on this system. If there is stil a log, you can continue.\nDo you want to continue? [y/n]${COL_RST}"
+      printf "\n${COL_YLW}###########\n${COL_YLW}# ${COL_RED}WARNING ${COL_YLW}#\n${COL_YLW}###########\n\n"
+      printf "${COL_BLE}Fail2Ban is not installed on this system. If there is stil a log, you can continue.\n${COL_BLE}Do you want to continue? [y/n]${COL_RST}"
       stty_cfg_t=$(stty -g)
       stty raw -echo ; input_t=$(head -c 1) ; stty $stty_cfg_t
       if echo "$input_t" | grep -iq "^n" ;then
@@ -49,8 +49,8 @@ fi
 if [[ ! -z "$@" ]]; then
    if [[ "$@" != "-p"* ]]; then
       if [[ "$@" != "--path"* ]]; then
-         printf "\n${COL_YLW}###########\n# ${COL_RED}WARNING ${COL_YLW}#\n###########\n\n${COL_BLE}"
-         printf "You entered an Invalid argument. Ignoring...\n${COL_RST}"
+         printf "\n${COL_YLW}###########\n${COL_YLW}# ${COL_RED}WARNING ${COL_YLW}#\n${COL_YLW}###########\n\n"
+         printf "${COL_BLE}You entered an Invalid argument. Ignoring...\n${COL_RST}"
       fi
    fi
 fi
@@ -60,8 +60,8 @@ do
    case $arg in
       -p|--path)
          F2B="$2"
-         printf "\n${COL_YLW}########\n# ${COL_BLE}INFO ${COL_YLW}#\n########\n\n${COL_GRN}"
-         printf "Path has been set to '${COL_BLE}%s${COL_GRN}'${COL_RST}\n" $F2B
+         printf "\n${COL_YLW}########\n${COL_YLW}# ${COL_BLE}INFO ${COL_YLW}#\n${COL_YLW}########\n\n"
+         printf "${COL_GRN}Path has been set to '${COL_BLE}%s${COL_GRN}'\n${COL_RST}" $F2B
          shift 
       ;;
       --default)
@@ -77,7 +77,7 @@ echo -e $'\nLoading...'
 if [ ! -f $F2B ]; then
    if [ ! -f $DEF ]; then
       if [[ $frc != 1 ]]; then
-         printf "\n${COL_YLW}#########\n# ${COL_RED}ERROR ${COL_YLW}#\n#########\n\n${COL_RST}"
+         printf "\n${COL_YLW}#########\n${COL_YLW}# ${COL_RED}ERROR ${COL_YLW}#\n${COL_YLW}#########\n\n${COL_RST}"
          printf "File not found!\nCheck the path in the script\n\nOr do you want me to search for the log? [y/n]\n\n${COL_RST}"
          stty_cfg=$(stty -g)
          stty raw -echo ; input=$(head -c 1) ; stty $stty_cfg
@@ -85,11 +85,11 @@ if [ ! -f $F2B ]; then
             printf "${COL_YLW}Searching... (Could take some time)${COL_RST}\n"
             LOC=$(find / -type f -iname "fail2ban.log")
             if [ "$LOC" '!=' '' ]; then
-               printf "\n${COL_YLW}########\n# ${COL_BLE}INFO ${COL_YLW}#\n########\n\n${COL_GRN}"
-               printf "File was found at %s!\n${COL_RST}" $LOC
+               printf "\n${COL_YLW}########\n${COL_YLW}# ${COL_BLE}INFO ${COL_YLW}#\n${COL_YLW}########\n\n"
+               printf "${COL_GRN}File was found at %s!\n${COL_RST}" $LOC
                F2B=$LOC
             else
-               printf "\n${COL_YLW}###########\n# ${COL_RED}WARNING ${COL_YLW}#\n###########\n\n${COL_RST}"
+               printf "\n${COL_YLW}###########\n${COL_YLW}# ${COL_RED}WARNING ${COL_YLW}#\n${COL_YLW}###########\n\n${COL_RST}"
                printf "File was not found!\n"
                exit 0
             fi
@@ -97,25 +97,25 @@ if [ ! -f $F2B ]; then
             exit 0
          fi
       else
-         printf "\n${COL_YLW}###########\n# ${COL_RED}WARNING ${COL_YLW}#\n###########\n\n${COL_RST}"
+         printf "\n${COL_YLW}###########\n${COL_YLW}# ${COL_RED}WARNING ${COL_YLW}#\n${COL_YLW}###########\n\n${COL_RST}"
          printf "File path incorrect. Forcing end!\n"
          exit 0
       fi
    else
-      printf "\n${COL_YLW}########\n# ${COL_BLE}INFO ${COL_YLW}#\n########\n\n${COL_RST}"
+      printf "\n${COL_YLW}########\n${COL_YLW}# ${COL_BLE}INFO ${COL_YLW}#\n${COL_YLW}########\n\n${COL_RST}"
       printf "Loaction '%s' not found.\n${COL_GRN}But file was found at %s\n(Default Path)\n${COL_RST}" $F2B $DEF
       F2B=$DEF
    fi
 fi
-printf "${COL_YLW}\n+----------------------------------------+\n| ${COL_GRN}FAIL2BAN IP ADRESS REPORT ${COL_RST}- "
+printf "\n${COL_YLW}+----------------------------------------+\n${COL_YLW}| ${COL_GRN}FAIL2BAN IP ADRESS REPORT ${COL_RST}- "
 printf "${COL_BLE}by NullDev ${COL_YLW}|\n"
 printf "+----------------------------------------+\n\n${COL_RST}"
-printf "${COL_YLW}########################\n#   ${COL_BLE}HOSTNAME ${COL_RST}AND ${COL_BLE}IP    "
-printf "${COL_YLW}#\n#----------------------#\n# ${COL_GRN}Format:              ${COL_YLW}#\n"
-printf "# ${COL_GRN}AMOUNT HOSTNAME (IP) ${COL_YLW}#\n########################\n\n${COL_RST}"
+printf "${COL_YLW}########################\n${COL_YLW}#   ${COL_BLE}HOSTNAME ${COL_RST}AND ${COL_BLE}IP    "
+printf "${COL_YLW}#\n${COL_YLW}#----------------------#\n${COL_YLW}# ${COL_GRN}Format:              ${COL_YLW}#\n"
+printf "# ${COL_GRN}AMOUNT HOSTNAME (IP) ${COL_YLW}#\n${COL_YLW}########################\n\n${COL_RST}"
 awk '($(NF-1) = /Ban/){print $NF,"("$NF")"}' $F2B | sort | logresolve | uniq -c | sort -n
-printf "${COL_YLW}\n########################\n#    ${COL_BLE}IP ${COL_RST}AND ${COL_BLE}EXPLOIT    "
-printf "${COL_YLW}#\n#----------------------#\n# ${COL_GRN}Format:              ${COL_YLW}#\n#"
+printf "${COL_YLW}\n${COL_YLW}########################\n${COL_YLW}#    ${COL_BLE}IP ${COL_RST}AND ${COL_BLE}EXPLOIT    "
+printf "${COL_YLW}#\n${COL_YLW}#----------------------#\n${COL_YLW}# ${COL_GRN}Format:              ${COL_YLW}#\n${COL_YLW}#"
 printf " ${COL_GRN}AMOUNT IP [EXPLOIT]  ${COL_YLW}#\n########################\n\n${COL_RST}"
 grep "Ban " $F2B | awk -F[\ \:] '{print $10,$8}' | sort | uniq -c | sort -n
 printf "\n"
